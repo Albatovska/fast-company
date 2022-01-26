24b0676f2d42ee74ed4c962c2c83793c2b7a666b
Tatiana <t.albatovskaya@gmail.com>
2022-01-26 22:08:20 +0200

    added ability to change user

--- src/componets/page/userEditPage/userEditPage.jsx
+++ src/componets/page/userEditPage/userEditPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import api from "../../../api";
 import TextField from "../../common/form/textField";
 import SelectField from "../../common/form/selectField";
@@ -9,23 +9,26 @@ import { validator } from "../../../utils/validator";
 
 const UserEditPage = () => {
     const { id } = useParams();
+    const histoty = useHistory();
     const [qualities, setQualities] = useState({});
     const [professions, setProfession] = useState();
     const [errors, setErrors] = useState({});
-    const [data, setData] = useState({});
+    const [data, setData] = useState({ isLoaded: false });
     useEffect(() => {
         api.users.getById(id).then((data) =>
             setData({
                 ...data,
-                profession: data.profession.name,
+                profession: data.profession._id,
                 qualities: data.qualities.map((quality) => ({
                     label: quality.name,
                     value: quality._id
-                }))
+                })),
+                isLoaded: true
             })
         );
         api.professions.fetchAll().then((data) => setProfession(data));
         api.qualities.fetchAll().then((data) => setQualities(data));
+
         return () => setData(null);
     }, []);
     const handleChange = (target) => {
@@ -69,71 +72,83 @@ const UserEditPage = () => {
         const isValid = validate();
         if (!isValid) return;
         console.log(data);
-    };
 
+        const userModified = {
+            ...data,
+            profession: Object.keys(professions)
+                .map((professionName) => professions[professionName])
+                .find((prof) => prof._id === data.profession),
+            qualities: Object.keys(qualities)
+                .map((qualityName) => qualities[qualityName])
+                .filter((quality) =>
+                    data.qualities.some((item) => item.value === quality._id)
+                )
+        };
+        console.log("userModified", userModified);
+        api.users.update(id, userModified).then(() => histoty.goBack());
+    };
+    if (!data.isLoaded) {
+        return <h1>Loading</h1>;
+    }
     return (
-        <>
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-md-6 offset-md-3 shadow p-4">
-                        {data ? (
-                            <form onSubmit={handleSubmit}>
-                                <TextField
-                                    label="Имя"
-                                    name="name"
-                                    value={data.name}
-                                    onChange={handleChange}
-                                    error={errors.name}
-                                />
-                                <TextField
-                                    label="Электронная почта"
-                                    name="email"
-                                    value={data.email}
-                                    onChange={handleChange}
-                                    error={errors.email}
-                                />
-                                <SelectField
-                                    label="Выбери свою профессию"
-                                    defaultOption={data.profession}
-                                    options={professions}
-                                    name="profession"
-                                    onChange={handleChange}
-                                    value={data.profession}
-                                    error={errors.profession}
-                                />
-                                <RadioField
-                                    options={[
-                                        { name: "Male", value: "male" },
-                                        { name: "Female", value: "female" },
-                                        { name: "Other", value: "other" }
-                                    ]}
-                                    value={data.sex}
-                                    name="sex"
-                                    onChange={handleChange}
-                                    label="Выберите ваш пол"
-                                />
-                                <MultiSelectField
-                                    label="Выберите ваши качества"
-                                    options={qualities}
-                                    defaultValue={data.qualities}
-                                    name="qualities"
-                                    onChange={handleChange}
-                                />
-                                <button
-                                    className="btn btn-primary w-100 mx-auto"
-                                    type="submit"
-                                    disabled={!isValid}
-                                >
-                                    Submit
-                                </button>
-                            </form>
-                        ) : (
-                            <h3>loading...</h3>
-                        )}
-                    </div>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    {data.isLoaded && (
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                label="Имя"
+                                name="name"
+                                value={data.name}
+                                onChange={handleChange}
+                                error={errors.name}
+                            />
+                            <TextField
+                                label="Электронная почта"
+                                name="email"
+                                value={data.email}
+                                onChange={handleChange}
+                                error={errors.email}
+                            />
+                            <SelectField
+                                label="Выбери свою профессию"
+                                defaultOption={data.profession}
+                                options={professions}
+                                name="profession"
+                                onChange={handleChange}
+                                value={data.profession}
+                                error={errors.profession}
+                            />
+                            <RadioField
+                                options={[
+                                    { name: "Male", value: "male" },
+                                    { name: "Female", value: "female" },
+                                    { name: "Other", value: "other" }
+                                ]}
+                                value={data.sex}
+                                name="sex"
+                                onChange={handleChange}
+                                label="Выберите ваш пол"
+                            />
+                            <MultiSelectField
+                                label="Выберите ваши качества"
+                                options={qualities}
+                                defaultValue={data.qualities}
+                                name="qualities"
+                                onChange={handleChange}
+                            />
+                            <button
+                                className="btn btn-primary w-100 mx-auto"
+                                type="submit"
+                                disabled={!isValid}
+                            >
+                                Submit
+                            </button>
+                        </form>
+                    )}
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
